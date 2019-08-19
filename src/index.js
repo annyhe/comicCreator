8ea0c88275ba9d54a707d9dd531bb29d826bba93
Anny He <anny.he@salesforce.com>
2019-08-19 13:39:40 -0700

    rectangle

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React, { Component, useState } from "react";
 import { render } from "react-dom";
-import { Stage, Layer, Image } from "react-konva";
+import { Stage, Layer, Image, Rect } from "react-konva";
 import useImage from "use-image";
 import Crop from "./cropIndex";
 
-// the first very simple and recommended way:
 const LionImage = (props) => {
   const [image] = useImage(props.url || "https://konvajs.org/assets/lion.png");
   return <Image draggable image={image} />;
 }; 
+
 class Images extends Component { 
   render() {
     const {blobs} = this.props;
@@ -19,6 +19,15 @@ class Images extends Component {
         <p>Prop Blobs: {blobs}</p>
         <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
+        <Rect
+            x={100}
+            y={100}
+            width={400}
+            height={600}
+            stroke="black"
+            draggable
+            fill="white"
+          />          
           { images }
         </Layer>
       </Stage>
