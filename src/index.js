b707ef79feb630956eae9160192e6d19f7afe375
Anny He <anny.he@salesforce.com>
2019-08-19 14:44:26 -0700

    one text per stage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component, useState } from "react";
 import { render } from "react-dom";
+import Portal from "./Portal"; // to render DOM nodes in Konva
 import {
   Stage,
   Layer,
@@ -7,7 +8,8 @@ import {
   Rect,
   Transformer,
   Group,
-  Text, Tag
+  Text,
+  Tag
 } from "react-konva";
 import useImage from "use-image";
 import Crop from "./cropIndex";
@@ -15,7 +17,7 @@ import Crop from "./cropIndex";
 const LionImage = props => {
   const [image] = useImage(props.url || "https://konvajs.org/assets/lion.png");
   return <Image draggable image={image} />;
-}; 
+};
 class EditableText extends Component {
   state = {
     selectedShapeName: "",
@@ -44,34 +46,10 @@ class EditableText extends Component {
       });
     }
   };
- 
-  render() {
-    return (
-      <div>
-        <Stage
-          width={window.innerWidth}
-          height={window.innerHeight}
-          onMouseDown={this.handleStageMouseDown}
-        >
-          <Layer>
 
-              <Text
-                draggable
-                name="text"
-                text={this.state.textValue}
-                x={225}
-                y={295}
-                fontSize={20}
-                onDblClick={this.handleTextDblClick}
-                fontSize={16}
-                fontFamily="Calibri"
-                fill="black"
-                width={220}
-                padding={15}
-                align="center"
-              />
-          </Layer>
-        </Stage>
+  render() {
+    return ( 
+      <Portal>
         <textarea
           value={this.state.textValue}
           style={{
@@ -83,15 +61,54 @@ class EditableText extends Component {
           onChange={this.handleTextEdit}
           onKeyDown={this.handleTextareaKeyDown}
         />
-      </div>
+
+      </Portal>
     );
   }
 }
 
 class KonvaStage extends Component {
+    state = {
+        selectedShapeName: "",
+        textEditVisible: false,
+        textX: 400,
+        textY: 400,
+        textValue: "Hello"
+      };
+      handleTextDblClick = e => {
+        const absPos = e.target.getAbsolutePosition();
+        this.setState({
+          textEditVisible: true,
+          textX: absPos.x,
+          textY: absPos.y
+        });
+      };
+      handleTextEdit = e => {
+        this.setState({
+          textValue: e.target.value
+        });
+      };
+      handleTextareaKeyDown = e => {
+        if (e.keyCode === 13) {
+          this.setState({
+            textEditVisible: false
+          });
+        }
+      };    
   render() {
     return (
       <div>
+                  <textarea
+          value={this.state.textValue}
+          style={{
+            display: this.state.textEditVisible ? "block" : "none",
+            position: "absolute",
+            top: this.state.textY + "px",
+            left: this.state.textX + "px"
+          }}
+          onChange={this.handleTextEdit}
+          onKeyDown={this.handleTextareaKeyDown}
+        />
         <Stage width={window.innerWidth} height={window.innerHeight}>
           <Layer>
             <Rect
@@ -104,6 +121,24 @@ class KonvaStage extends Component {
               fill="white"
             />
             {this.props.children}
+
+
+      <Text
+          draggable
+          name="text"
+          text={this.state.textValue}
+          x={225}
+          y={295}
+          fontSize={20}
+          onDblClick={this.handleTextDblClick}
+          fontSize={16}
+          fontFamily="Calibri"
+          fill="black"
+          width={220}
+          padding={15}
+          align="center"
+        />
+
           </Layer>
         </Stage>
       </div>
@@ -123,15 +158,14 @@ class App extends Component {
   render() {
     const { blobs } = this.state;
     const images = blobs.map((blob, index) => (
-        <LionImage key={index} url={blob} alt={"cropped image " + index} />
-      ));    
+      <LionImage key={index} url={blob} alt={"cropped image " + index} />
+    ));
     return (
       <div>
         <Crop saveCroppedImage={this.saveCroppedImage} />
         <KonvaStage>
-            {images}
-            {/* <EditableText /> */}
-            </KonvaStage>
+          {images}
+        </KonvaStage>
       </div>
     );
   }
