10e85b1b1474215e0c939509e515dcbaa51804ea
Anny He <anny.he@salesforce.com>
2019-08-21 10:39:24 -0700

    POST to cloudinary works

--- READme.md
+++ READme.md
@@ -1,9 +1,18 @@
 DONE
 - save images to google drive, with base64 from POST body
 - load images from google drive
+- post to cloudinary
 
 TODO
-
+- connect server and client on 4 REST requests
+- add Salesforce to express:
+    - POST: create new comic record, post json
 
 BUGS
+- CORB on accessing google drive file from Salesforce, causing image broken if formula(googleDriveURL). alternatives
+- use google drive connect https://gyansys.com/2017/06/integrating-salesforce-google-drive-using-files-connect/
+- Lighting component: get file from google drive
+- Store files in salesforce, instead of google drive :(
+- store files in cloudinary instead of google drive??? :) can see the image. :( How to send sketches to cloudinary? upload via web app?
+
 - for text only: load stage, change something, load stage, expect to revert to loaded state, yet nothing changed
--- app.js
+++ app.js
@@ -1,46 +1,70 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cloudinary = require("cloudinary");
 const app = express();
-const authList = require('./googleDrive').authList;
-const listFiles = require('./googleDrive').listFiles;
-const uploadFile = require('./googleDrive').uploadFile;
-
+const authList = require("./googleDrive").authList;
+const listFiles = require("./googleDrive").listFiles;
+const uploadFile = require("./googleDrive").uploadFile;
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // parse request body
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
-// get google drive sketchobook folder files
-app.get('/api/getList', (req,res) => {
-    authList()
-    .then((oAuth2Client) => listFiles(oAuth2Client))
-    .then((files) => {
-            res.json(files);
-    })
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+);
+
+app.post("/api/saveToCloudinary", (req, res) => {
+  console.log(req.body);
+  const { name, data } = req.body;
+
+  cloudinary.v2.uploader.upload(
+    data,
+    {
+      overwrite: true,
+      invalidate: true
+    },
+    function(error, result) {
+      if (result) {
+        res.json(result.secure_url);
+      } else if (error) {
+        console.log("Error from saveToCloudinary", error);
+      }
+    }
+  );
 });
 
-app.post('/api/postImage', (req,res) => {
-    console.log(req.body);
-    const {name, data} = req.body;
-    authList()
-    .then((oAuth2Client) => uploadFile(oAuth2Client, name, data))
-    .then((fileID) => {
-        if (fileID) {
-            res.json(fileID);
-        }
-    })
+// get google drive sketchobook folder files
+app.get("/api/getList", (req, res) => {
+  authList()
+    .then(oAuth2Client => listFiles(oAuth2Client))
+    .then(files => {
+      res.json(files);
+    });
+});
+
+app.post("/api/postImage", (req, res) => {
+  console.log(req.body);
+  const { name, data } = req.body;
+  authList()
+    .then(oAuth2Client => uploadFile(oAuth2Client, name, data))
+    .then(fileID => {
+      if (fileID) {
+        res.json(fileID);
+      }
+    });
 });
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('App is listening on port ' + port);
+console.log("App is listening on port " + port);
